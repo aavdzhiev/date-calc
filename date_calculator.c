a8b6abeb292ddad0ab03cd802adb15dfe1815875
PetarPetrov5 <76947503+PetarPetrov5@users.noreply.github.com>
2021-01-23 14:46:19 +0200

    calculateDayOfTheWeek (#1)
    * calculateDayOfTheWeek
    
    * Update date_calculator.c
    
    * Update date_calculator.c

--- date_calculator.c
+++ date_calculator.c
@@ -9,7 +9,7 @@
 // output %d.%d.%dddd се пада събота.
 //        Този месец има %d дни.
 //        Годината (не) е високосна.
-short int leapYear(int year);
+const char* calculateDayOfTheWeek(short int day, short int month, short int year);
 
 int main(void)
 {
@@ -20,10 +20,51 @@ int main(void)
         short int day, month, year;
         printf("Enter Day, Month, and Year as DD MM YYYY: \n");
         scanf("%2hu%2hu%4hu", &day, &month, &year);
-
-        printf("%hu,%hu,%hu\n", day, month, year);
-
-    } while (i == 'y');
+        printf("%s\n", calculateDayOfTheWeek(day, month, year));
+        printf("Do you want to enter another date: (Y/N)");
+        scanf(" %c", &i);
+    } while (i == 'y' || i == 'Y');
 
     return 0;
+}
+
+const char* calculateDayOfTheWeek(short int day, short int month, short int year){
+    short int firstTwoDigitsOfYear = year/100;
+    short int lastTwoDigitsOfYear = year%100;
+    if (month == 1) {
+        month = 13;
+        year--;
+    }
+    if (month == 2) {
+        month = 14;
+        year--;
+    }
+    //Zeller’s Congruence
+    short int dayOfWeek = (day + 13 * (month + 1) / 5 + lastTwoDigitsOfYear + lastTwoDigitsOfYear/4 + firstTwoDigitsOfYear/4 - 5*firstTwoDigitsOfYear) % 7;
+    switch(dayOfWeek){
+        case 1:
+            return "Sunday";
+        break;
+        case 2:
+            return "Monday";
+        break;
+        case 3:
+            return "Tuesday";
+        break;
+        case 4:
+            return "Wednesday";
+        break;
+        case 5:
+            return "Thursday";
+        break;
+        case 6:
+            return "Friday";
+        break;
+        case 0:
+            return "Saturday";
+        break;
+        default: 
+            return "Error";
+        break;
+    }
 }
