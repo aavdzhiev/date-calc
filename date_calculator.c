7d0056923c761afe27f7b5ffc4fbdd595d34af19
PetarPetrov5 <76947503+PetarPetrov5@users.noreply.github.com>
2021-01-23 16:00:42 +0200

    Update date_calculator.c (#4)

--- date_calculator.c
+++ date_calculator.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-const char* calculateDayOfTheWeek(short int day, short int month, short int year);
+const char *calculateDayOfTheWeek(short int day, short int month, short int year);
 short int isLeap(short int year);
 short int months_day(short int month, short int year);
-        
+void inputOutput();
+
 int main(void)
 {
     char i;
@@ -13,17 +14,24 @@ int main(void)
         short int day, month, year;
         printf("Enter Day, Month, and Year as DD MM YYYY: \n");
         scanf("%2hu%2hu%4hu", &day, &month, &year);
-        printf("%s\n", calculateDayOfTheWeek(day, month, year));
-        printf("Do you want to enter another date: (Y/N)\n");
-        scanf(" %c", &i);
+        if (day < 0 || day > 31 || month < 0 || month > 12 ) {
+            printf("Please enter a valid date.\n");
+            printf("Do you want to enter another date: (Y/N)\n");
+            scanf(" %c", &i);
+        }
+        else {
+            printf("%hu.%hu.%hu is %s and the month has %hu days\n", day, month, year, calculateDayOfTheWeek(day, month, year), months_day(month, year));
+            printf("Do you want to enter another date: (Y/N)\n");
+            scanf(" %c", &i); 
+        }
     } while (i == 'y' || i == 'Y');
 
     return 0;
 }
 
-const char* calculateDayOfTheWeek(short int day, short int month, short int year){
-    short int firstTwoDigitsOfYear = year/100;
-    short int lastTwoDigitsOfYear = year%100;
+const char *calculateDayOfTheWeek(short int day, short int month, short int year) {
+    short int firstTwoDigitsOfYear = year / 100;
+    short int lastTwoDigitsOfYear = year % 100;
     if (month == 1) {
         month = 13;
         year--;
@@ -33,31 +41,31 @@ const char* calculateDayOfTheWeek(short int day, short int month, short int year
         year--;
     }
     //Zeller’s Congruence
-    short int dayOfWeek = (day + 13 * (month + 1) / 5 + lastTwoDigitsOfYear + lastTwoDigitsOfYear/4 + firstTwoDigitsOfYear/4 - 5*firstTwoDigitsOfYear) % 7;
-    switch(dayOfWeek){
-        case 1:
-            return "Sunday";
+    short int dayOfWeek = (day + 13 * (month + 1) / 5 + lastTwoDigitsOfYear + lastTwoDigitsOfYear / 4 + firstTwoDigitsOfYear / 4 - firstTwoDigitsOfYear) % 7;
+    switch (dayOfWeek) {
+    case 1:
+        return "Sunday";
         break;
-        case 2:
-            return "Monday";
+    case 2:
+        return "Monday";
         break;
-        case 3:
-            return "Tuesday";
+    case 3:
+        return "Tuesday";
         break;
-        case 4:
-            return "Wednesday";
+    case 4:
+        return "Wednesday";
         break;
-        case 5:
-            return "Thursday";
+    case 5:
+        return "Thursday";
         break;
-        case 6:
-            return "Friday";
+    case 6:
+        return "Friday";
         break;
-        case 0:
-            return "Saturday";
+    case 0:
+        return "Saturday";
         break;
-        default: 
-            return "Error";
+    default:
+        return "Error";
         break;
     }
 }
@@ -65,24 +73,24 @@ const char* calculateDayOfTheWeek(short int day, short int month, short int year
 short int isLeap(short int year) {
     if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
         return 1;
-    } else {
+    }
+    else {
         return 0;
     }
 }
 
-short int months_day(short int month, short int year){
-        short int days = 31;
-        
-        if (month == 4 || month == 6 || month == 9 || month == 11)
-                days = 30;
+short int months_day(short int month, short int year) {
+    short int days = 31;
 
-        else if (month == 2) {
-                if ((((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) == 0)
-                        days = 29;
-                else
-                        days = 28;                
-        }
+    if (month == 4 || month == 6 || month == 9 || month == 11)
+        days = 30;
 
-        
-        return days;
+    else if (month == 2) {
+        if (isLeap(year))
+            days = 29;
+        else
+            days = 28;
+    }
+
+    return days;
 }
