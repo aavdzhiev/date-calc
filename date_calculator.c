a3ba9c981773ccc15c839fd90fa357e4c95ffe0c
Nikolay <67786095+zzzlooper@users.noreply.github.com>
2021-01-24 23:51:04 +0200

    cleanup (#8)
    Co-authored-by: Nikolay <looper@riseup.net>

--- date_calculator.c
+++ date_calculator.c
@@ -4,18 +4,15 @@ const char *calculateDayOfTheWeek(short int day, short int month, short int year
 short int isLeap(short int year);
 short int months_day(short int month, short int year);
 
-int main(void)
-{
+int main (void) {
     char i;
 
-    do
-    {
+    do {
         short int day, month, year;
         printf("Enter Day, Month, and Year as DD MM YYYY: \n");
         scanf("%2hu%2hu%4hu", &day, &month, &year);
         if (day <= 0 || day > 31 || month <= 0 || month > 12 || year <= 0 || 
         ( isLeap(year) && day > 29 && month == 2 ) || ( !isLeap(year) && day > 28 && month==2) ) {
-
             printf("Please enter a valid date.\n");
             printf("Do you want to enter another date: (Y/N)\n");
             scanf(" %c", &i);
@@ -31,8 +28,6 @@ int main(void)
 }
 
 const char *calculateDayOfTheWeek(short int day, short int month, short int year) {
-    short int firstTwoDigitsOfYear = year / 100;
-    short int lastTwoDigitsOfYear = year % 100;
     if (month == 1) {
         month = 13;
         year--;
@@ -42,7 +37,6 @@ const char *calculateDayOfTheWeek(short int day, short int month, short int year
         year--;
     }
     //Zeller’s Congruence
-    //short int dayOfWeek = (day + 13 * (month + 1) / 5 + lastTwoDigitsOfYear + lastTwoDigitsOfYear / 4 + firstTwoDigitsOfYear / 4 - firstTwoDigitsOfYear) % 7;
     short int dayOfWeek = (day + 13 * (month + 1) / 5 + year + year / 4 - year / 100 + year / 400) % 7;
     switch (dayOfWeek) {
     case 1:
@@ -73,12 +67,10 @@ const char *calculateDayOfTheWeek(short int day, short int month, short int year
 }
 
 short int isLeap(short int year) {
-    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
         return 1;
-    }
-    else {
-        return 0;
-    }
+    else
+        return 0;    
 }
 
 short int months_day(short int month, short int year) {
